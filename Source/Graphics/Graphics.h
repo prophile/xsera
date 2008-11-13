31cff123954208b48d94bf356f939c0828341957
Alistair Lynn <arplynn@gmail.com>
2008-11-13 00:41:12 +0000

    Added initial Sound implementation.
    Mostly just stubs, also did some tweaking to the graphics header file by removing a newline that shouldn't have been there.

--- Source/Graphics/Graphics.h
+++ Source/Graphics/Graphics.h
@@ -6,13 +6,12 @@
 namespace Graphics
 {
 
-void Init ( int w, int h, int bpp, bool fullscreen );
+void Init ( int w, int h, bool fullscreen );
 
 void DrawSprite ( const std::string& sheetname, int sheet_x, int sheet_y, vec2 location, vec2 size, float rotation );
 void DrawText ( const std::string& text, const std::string& font, int fontsize, colour col, float rotation );
 void DrawLine ( vec2 coordinate1, vec2 coordinate2, float width, colour col );
 void DrawCircle ( vec2 centre, float radius, float width, colour col );
-
 void DrawParticles ( const vec2* locations, unsigned int count, colour col );
 
 void SetCamera ( vec2 corner1, vec2 corner2, float rotation );
---
+++ Source/Sound/Sound.cpp
@@ -0,0 +1,63 @@
+#include "Sound.h"
+#include <SDL/SDL.h>
+#include <SDL_mixer/SDL_mixer.h>
+
+namespace Sound
+{
+
+void Init ( int frequency, int resolution, int sources )
+{
+	int volume_sound = MIX_MAX_VOLUME / 2, volume_music = MIX_MAX_VOLUME / 2;
+	Uint16 format;
+	switch (resolution)
+	{
+		case 8:
+			format = AUDIO_S8;
+			break;
+		case 16:
+			format = AUDIO_S16SYS;
+			break;
+		default:
+			printf("Unsupported audio format, defaulting to 16-bit");
+			format = AUDIO_S16SYS;
+			break;
+	}
+	Mix_OpenAudio(frequency, format, 2, 4096);
+	Mix_AllocateChannels(sources);
+	Mix_VolumeMusic(volume_music);
+	Mix_Volume(-1, volume_sound);
+}
+
+void PlaySound ( const std::string& name, float gain, float pan )
+{
+}
+
+void PlayMusic ( const std::string& music )
+{
+}
+
+void StopMusic ()
+{
+}
+
+float MusicVolume ()
+{
+	return Mix_VolumeMusic(-1) / (float)MIX_MAX_VOLUME;
+}
+
+void SetMusicVolume ( float _mvol )
+{
+	Mix_VolumeMusic(_mvol * MIX_MAX_VOLUME);
+}
+
+float SoundVolume ()
+{
+	return Mix_Volume(-1, -1) / (float)MIX_MAX_VOLUME;
+}
+
+void SetSoundVolume ( float _mvol )
+{
+	Mix_Volume(-1, MIX_MAX_VOLUME * _mvol);
+}
+
+}
