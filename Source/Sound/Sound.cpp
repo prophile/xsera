8edffec0a94e2aada26bf70db4703d0e0e20de3c
Alistair Lynn <arplynn@gmail.com>
2008-11-13 11:12:15 +0000

    Finished the Sound engine.
    Finished up work on the sound engine, with support for AIFF sounds and S3M and OGG music. They should be in the Sound and Music directories respectively.

--- Source/Sound/Sound.cpp
+++ Source/Sound/Sound.cpp
@@ -1,6 +1,9 @@
 #include "Sound.h"
 #include <SDL/SDL.h>
 #include <SDL_mixer/SDL_mixer.h>
+#include <map>
+#include <math.h>
+#include "Utilities/ResourceManager.h"
 
 namespace Sound
 {
@@ -8,18 +11,55 @@ namespace Sound
 namespace Internal
 {
 
+typedef std::map<std::string, Mix_Chunk*> EffectsMap;
+typedef std::map<std::string, Mix_Music*> MusicMap;
+EffectsMap effects;
+MusicMap musics;
+
 void ClearSoundAndMusic ()
 {
 }
 
 Mix_Chunk* SoundNamed ( const std::string& name )
 {
-	return NULL;
+	EffectsMap::iterator iter = effects.find(name);
+	if (iter == effects.end())
+	{
+		// load the sound
+		std::string path = "Sounds/" + name + ".aiff";
+		SDL_RWops* ops = ResourceManager::OpenFile(path);
+		Mix_Chunk* newChunk = Mix_LoadWAV_RW(ops, 1);
+		effects[path] = newChunk;
+		return newChunk;
+	}
+	else
+	{
+		return iter->second;
+	}
 }
 
 Mix_Music* MusicNamed ( const std::string& name )
 {
-	return NULL;
+	MusicMap::iterator iter = musics.find(name);
+	if (iter == musics.end())
+	{
+		// load the sound
+		std::string path = "Music/" + name + ".s3m";
+		SDL_RWops* ops = ResourceManager::OpenFile(path);
+		Mix_Music* newMusic = Mix_LoadMUS_RW(ops);
+		if (!newMusic)
+		{
+			std::string path = "Music/" + name + ".ogg";
+			SDL_RWops* ops = ResourceManager::OpenFile(path);
+			newMusic = Mix_LoadMUS_RW(ops);
+		}
+		musics[path] = newMusic;
+		return newMusic;
+	}
+	else
+	{
+		return iter->second;
+	}
 }
 
 }
@@ -51,14 +91,32 @@ void Init ( int frequency, int resolution, int sources )
 
 void PlaySound ( const std::string& name, float gain, float pan )
 {
+	Mix_Chunk* chunk = SoundNamed(name);
+	if (!chunk)
+		return;
+	Uint8 ipan = pan * 127;
+	int ivolume = gain * (MIX_MAX_VOLUME / 2);
+	int channel = Mix_GroupAvailable(-1);
+	Mix_UnregisterAllEffects(channel);
+	if (ipan != 127)
+		Mix_SetPanning(channel, 0xFF - ipan, ipan);
+	if (fabs(gain - 1.0f) > 0.0003f)
+		Mix_SetDistance(channel, 1 / gain);
+	Mix_PlayChannel(channel, chunk, 0);
 }
 
 void PlayMusic ( const std::string& music )
 {
+	Mix_Music* mus = MusicNamed(music);
+	if (mus)
+	{
+		Mix_PlayMusic(mus, -1);
+	}
 }
 
 void StopMusic ()
 {
+	Mix_HaltMusic();
 }
 
 float MusicVolume ()
