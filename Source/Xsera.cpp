7efc3838e243e12eecbb0904abb69762eb2d341d
Alistair Lynn <arplynn@gmail.com>
2008-11-13 11:17:00 +0000

    Added init and resource management to Xsera.cpp
    Xsera.cpp will now init the resource manager, graphics and sound subsystems with constant values, and will poll events.

--- Source/Xsera.cpp
+++ Source/Xsera.cpp
@@ -1,7 +1,20 @@
 #include <SDL/SDL.h>
+#include "Utilities/ResourceManager.h"
+#include "Graphics/Graphics.h"
+#include "Sound/Sound.h"
 
 namespace
 {
+
+void DispatchEvent ( const SDL_Event& evt )
+{
+	switch (evt.type)
+	{
+		case SDL_QUIT:
+			exit(0);
+			break;
+	}
+}
 
 void RunLoop ()
 {
@@ -11,12 +24,18 @@ void RunLoop ()
 void Startup ()
 {
 	// do init stuff
+	ResourceManager::Init();
+	Graphics::Init(640, 480, false); // 640x480 resolution, non-fullscreen
+	Sound::Init(48000, 24, 128); // init with 48 kHz sampling rate, 24-bit resolution, 128 channels
 }
 
 void MainLoop ()
 {
 	while (true)
 	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event) > 0)
+			DispatchEvent(event);
 		RunLoop();
 		SDL_Delay(1);
 	}
