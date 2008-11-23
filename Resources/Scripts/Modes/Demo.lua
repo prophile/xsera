7dc9c9ac9c3240e6a49c8f96562ffc0c8a074f5f
Adam Hintz <adam.0.dev@gmail.com>
2008-11-22 20:01:27 -0800

    Side panels display correctly. Input accepts WASD controls as strafe commands. No turning ability.

--- Resources/Scripts/Modes/Demo.lua
+++ Resources/Scripts/Modes/Demo.lua
@@ -12,24 +12,46 @@
 
 spriteSheetX = 2
 spriteSheetY = 3
-screenSizeX = { min = 0, max = 100 } -- temporary values
-screenSizeY = { min = 0, max = 100 } -- temporary values
+screenSizeX = { min = 0, max = 100 } -- temporary values, unused right now
+screenSizeY = { min = 0, max = 100 } -- temporary values, unused right now
 
 ships = {}
-camera = { -500, -240, 500, 240 }
+camera = { -500, -500, 500, 500 }
 carrierLocation = { 100, 50 }
 carrierSize = { 70, 49 }
-hCruiserLocation = { -100, 0 }
+hCruiserLocation = { 0, 0 }
 hCruiserSize = { 35, 31 }
 
 function render ()
     graphics.begin_frame()
     
     graphics.set_camera(camera[1], camera[2], camera[3], camera[4])
-    graphics.draw_image("Panels/SideLeft", -430, 1, 129, 480)
-    graphics.draw_image("Panels/SideRight", 430, 1, 129, 480)
+    graphics.draw_image("Panels/SideLeft", camera[1] + 68, camera[2] + 501, 129, 1000)
+    graphics.draw_image("Panels/SideRight", camera[3] - 14, camera[2] + 501, 27, 1000)
     graphics.draw_sprite("Gaitori/Carrier", carrierLocation[1], carrierLocation[2], carrierSize[1], carrierSize[2])
     graphics.draw_sprite("Ishiman/HeavyCruiser", hCruiserLocation[1], hCruiserLocation[2], hCruiserSize[1], hCruiserSize[2])
 	
     graphics.end_frame()
+end
+
+function key ( k )
+	if k == "w" then
+		camera[2] = camera[2] + 1
+		camera[4] = camera[4] + 1
+		hCruiserLocation[2] = hCruiserLocation[2] + 1;
+	elseif k == "s" then
+		camera[2] = camera[2] - 1
+		camera[4] = camera[4] - 1
+		hCruiserLocation[2] = hCruiserLocation[2] - 1;
+	elseif k == "a" then
+		camera[1] = camera[1] - 1
+		camera[3] = camera[3] - 1
+		hCruiserLocation[1] = hCruiserLocation[1] - 1;
+	elseif k == "d" then
+		camera[1] = camera[1] + 1
+		camera[3] = camera[3] + 1
+		hCruiserLocation[1] = hCruiserLocation[1] + 1;
+	else
+        print("Uninterpreted keystroke '" .. k .. "'")
+	end
 end
