42ba634e00e5e6f92b47b515760a5ae2c862554d
Alastair Lynn <arplynn@gmail.com>
2008-12-20 19:29:23 +0000

    Updated the credits system to read from a config file

--- Resources/Scripts/Modes/Credits.lua
+++ Resources/Scripts/Modes/Credits.lua
@@ -1,42 +1,36 @@
 creditsRolling = true
 totalTime = 0.0
-regularDist = -32
-titleDist = -40
-headerDist = -35
-mainDist = -50
 initialDist = -200
+separationDist = 10
 speed = 25
 
+credits = {}
+creditSizes = {}
 rowDist = {}
-rowDist[1] = initialDist
-rowDist[2] = rowDist[1] + mainDist
-rowDist[3] = rowDist[2] + regularDist
-rowDist[4] = rowDist[3] + regularDist
 
-rowDist[5] = rowDist[4] + titleDist
-rowDist[6] = rowDist[5] + regularDist
-rowDist[7] = rowDist[6] + regularDist
-rowDist[8] = rowDist[7] + regularDist
-
-rowDist[9] = rowDist[8] + titleDist
-rowDist[10] = rowDist[9] + regularDist
-
-rowDist[11] = rowDist[10] + titleDist
-rowDist[12] = rowDist[11] + regularDist
-
-rowDist[13] = rowDist[12] + titleDist
-rowDist[14] = rowDist[13] + regularDist
-rowDist[15] = rowDist[14] + headerDist
-rowDist[16] = rowDist[15] + regularDist
+function load_credits ()
+	local rawInput = resource_manager.load("Config/Credits.txt")
+	local i = 1
+	local lastDist = initialDist
+	for sizeString, line in string.gmatch(rawInput, "(%**)(.-)\n") do
+		credits[i] = line
+		creditSizes[i] = 30 + (10 * #sizeString)
+		rowDist[i] = lastDist
+		lastDist = lastDist - separationDist - creditSizes[i]
+		i = i + 1
+	end
+end
 
 function init ()
-    sound.play_music("Doomtroopers")
+	sound.play_music("Doomtroopers")
 	oldTime = mode_manager.time()
+	-- load credits
+	load_credits()
 end
 
 function key ( k )
 	if k == 'p' then
-		if creditsRolling  == true then
+		if creditsRolling	 == true then
 			creditsRolling = false
 		else
 			creditsRolling = true
@@ -51,26 +45,11 @@ end
 function render ()
 	graphics.begin_frame()
 	graphics.set_camera(-320, -240, 320, 240)
-	graphics.draw_text("Xsera", "CrystalClear", 0, rowDist[1] + totalTime, 60)
-	graphics.draw_text("A Brainpen Production", "CrystalClear", 0, rowDist[2] + totalTime, 30)
-	graphics.draw_text("Based on the game 'Ares' by Nathan Lamont", "CrystalClear", 0, rowDist[3] + totalTime, 30)
-	graphics.draw_text("Copyright 1997-2001, 2008", "CrystalClear", 0, rowDist[4] + totalTime, 30)
-	
-	graphics.draw_text("Programmers", "CrystalClear", 0, rowDist[5] + totalTime, 40)
-	graphics.draw_text("Alistair 'Prophile' Lynn", "CrystalClear", 0, rowDist[6] + totalTime, 30)
-	graphics.draw_text("Adam 'adam_0' Hintz", "CrystalClear", 0, rowDist[7] + totalTime, 30)
-	graphics.draw_text("Andrew 'LANS' Moscoe", "CrystalClear", 0, rowDist[8] + totalTime, 30)
-	
-	graphics.draw_text("Coordinator", "CrystalClear", 0, rowDist[9] + totalTime, 40)
-	graphics.draw_text("Nick 'cia_man' Farley", "CrystalClear", 0, rowDist[10] + totalTime, 30)
-	
-	graphics.draw_text("Contact to Nathan Lamont", "CrystalClear", 0, rowDist[11] + totalTime, 40)
-	graphics.draw_text("'redsteven'", "CrystalClear", 0, rowDist[12] + totalTime, 30)
-	
-	graphics.draw_text("Graphics Artists", "CrystalClear", 0, rowDist[13] + totalTime, 40)
-	graphics.draw_text("All original graphics copyright Nathan Lamont", "CrystalClear", 0, rowDist[14] + totalTime, 30)
-	graphics.draw_text("Weapons and Shields", "CrystalClear", 0, rowDist[15] + totalTime, 35)
-	graphics.draw_text("'DarkRevenant'", "CrystalClear", 0, rowDist[16] + totalTime, 30)
+	for i, credit in pairs(credits) do
+		if credit ~= "" then
+			graphics.draw_text(credit, "CrystalClear", 0, rowDist[i] + totalTime, creditSizes[i])
+		end
+	end
 	graphics.end_frame()
 end
 
@@ -79,9 +58,9 @@ function update ()
 	local dt = newTime - oldTime
 	oldTime = newTime
 	if creditsRolling then
-	   totalTime = totalTime + speed * dt
-    end
-	if totalTime > -rowDist[16] + 300 then
-        mode_manager.switch("MainMenu")
+		totalTime = totalTime + speed * dt
 	end
+	-- if totalTime > -rowDist[table.maxn(rowDist)] + 300 then
+	--	mode_manager.switch("MainMenu")
+	-- end
 end
