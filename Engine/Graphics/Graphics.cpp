4f013b27b7bdb924b8a7d4c04cbd38382796cf31
Adam Hintz <adam.0.dev@gmail.com>
2009-03-06 20:56:07 -0800

    Disabled shaders. I hope this works, because I'm seeing nothing now on my side.

--- Engine/Graphics/Graphics.cpp
+++ Engine/Graphics/Graphics.cpp
@@ -285,7 +285,7 @@ vec2 SpriteDimensions ( const std::string& sheetname )
 
 void DrawSprite ( const std::string& sheetname, int sheet_x, int sheet_y, vec2 location, vec2 size, float rotation )
 {
-	SetShader("Sprite");
+//	SetShader("Sprite");
 	EnableTexturing();
 	EnableBlending();
 	ClearColour();
@@ -318,7 +318,7 @@ void DrawSprite ( const std::string& sheetname, int sheet_x, int sheet_y, vec2 l
 
 void DrawTextSDL ( const std::string& text, const std::string& font, vec2 location, float height, colour col, float rotation )
 {
-	SetShader("Sprite");
+//	SetShader("Sprite");
 	EnableTexturing();
 	EnableBlending();
 	SetColour(col);
@@ -338,7 +338,7 @@ void DrawTextSDL ( const std::string& text, const std::string& font, vec2 locati
 
 void DrawLine ( vec2 coordinate1, vec2 coordinate2, float width, colour col )
 {
-	SetShader("Primitive");
+//	SetShader("Primitive");
 	DisableTexturing();
 	if (col.alpha() < 1.0f)
 	{
@@ -360,7 +360,7 @@ void DrawLine ( vec2 coordinate1, vec2 coordinate2, float width, colour col )
 
 void DrawCircle ( vec2 centre, float radius, float width, colour col )
 {
-	SetShader("Primitive");
+//	SetShader("Primitive");
 	DisableTexturing();
 	if (col.alpha() < 1.0f)
 	{
@@ -380,7 +380,7 @@ void DrawCircle ( vec2 centre, float radius, float width, colour col )
 
 void DrawParticles ( const vec2* locations, unsigned int count, colour col )
 {
-	SetShader("Primitive");
+//	SetShader("Primitive");
 	DisableTexturing();
 	if (col.alpha() < 1.0f)
 	{
