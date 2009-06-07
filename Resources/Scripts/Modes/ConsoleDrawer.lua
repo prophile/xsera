74bbbafadb21350d118ec9637f12c31ba72fa774
Adam Hintz <adam.0.dev@gmail.com>
2009-06-07 12:25:47 -0700

    Console now has implemented command system. Typing 'load demo' or 'return' will load Demo2 and return to the main screen, respectively. This will be very helpful in the future, although it has little value right now.
    Signed-off-by: Adam Hintz <adam.0.dev@gmail.com>

--- Resources/Scripts/Modes/ConsoleDrawer.lua
+++ Resources/Scripts/Modes/ConsoleDrawer.lua
@@ -14,7 +14,7 @@ end
 function render ()
     graphics.begin_frame()
 	graphics.set_camera(-300, -200, 300, 200)
-	i = 1
+	local i = 1
 	while consoleHistory[i] ~= nil do
 		if i <= CONSOLE_MAX then
 			graphics.draw_text(consoleHistory[i], "CrystalClear", "left", -295, 193 - (i - 1) * 22, 20)
@@ -29,6 +29,7 @@ end
 
 function key ( k )
 	if k == "return" then
+		line_handle()
 		line = line + 1
 		table.insert(consoleHistory, ">")
 		return
@@ -50,5 +51,17 @@ function update ()
 	while line > CONSOLE_MAX do
 		table.remove(consoleHistory, 1)
 		line = line - 1
+	end
+end
+
+function line_handle()
+	local i = 1
+	while commands[i] ~= nil do
+		print(commands[i])
+		if ">" .. commands[i][1] == consoleHistory[line] then
+			commands[i][2]()
+			return
+		end
+		i = i + 1
 	end
 end
--- Resources/Scripts/Modules/Console.lua
+++ Resources/Scripts/Modules/Console.lua
@@ -1,6 +1,24 @@
 consoleHistory = {}
 line = 0
 
+
+function c_return()
+	mode_manager.switch("MainMenu")
+end
+
+function c_load()
+	
+end
+
+function sc_load_demo()
+	mode_manager.switch("Demo2")
+end
+
+commands = {
+			{ "return", c_return },
+			{ "load demo", sc_load_demo } }
+
+
 function console_add(text)
 	table.insert(consoleHistory, text)
 	line = line + 1
