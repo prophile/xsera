cac53ffacab1f8827b0ab0cb09dd6cf61e815ab0
Adam Hintz <adam.0.dev@gmail.com>
2009-07-06 23:17:30 -0700

    Updated ares_splash.lua to contain character options for playing or quitting.
    Signed-off-by: Adam Hintz <adam.0.dev@gmail.com>

--- Resources/Scripts/Modes/ares_splash.lua
+++ Resources/Scripts/Modes/ares_splash.lua
@@ -1,3 +1,8 @@
+splash_shift = -135
+top_of_splash = -14
+fontsize = 18
+splash_num = 0
+
 function init()
 	sound.stop_music()
 end
@@ -8,14 +13,26 @@ end
 
 function render()
 	graphics.begin_frame()
+	splash_num = 0
 	graphics.set_camera(-320, -240, 320, 240)
     graphics.draw_image("Panels/MainTop", 0, 118, 640, 245)
     graphics.draw_image("Panels/MainBottom", 0, -227, 640, 24)
     graphics.draw_image("Panels/MainLeft", -231, -110, 178, 211)
     graphics.draw_image("Panels/MainRight", 230, -110, 180, 211)
+	graphics.draw_text("P - Play", "CrystalClear", "left", splash_shift, top_of_splash + splash_num * (-fontsize - 1), fontsize)
+	splash_num = splash_num + 1
+	graphics.draw_text("M - Xsera Main Menu", "CrystalClear", "left", splash_shift, top_of_splash + splash_num * (-fontsize - 1), fontsize)
+	splash_num = splash_num + 1
+	graphics.draw_text("Q - Quit", "CrystalClear", "left", splash_shift, top_of_splash + splash_num * (-fontsize - 1), fontsize)
 	graphics.end_frame()
 end
 
 function key(k)
-	
+	if k == "p" then
+		mode_manager.switch("Demo2")
+	elseif k == "m" then
+		mode_manager.switch("MainMenu")
+	elseif k == "q" then
+		os.exit()
+	end
 end
