bd5491a935ce92e66b9fcda2494cdd7ad47db22d
Alastair Lynn <arplynn@gmail.com>
2009-11-13 04:40:01 +0000

    Cleaned up the message decoding code a lot.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- Engine/Net/MessageDecode.cpp
+++ Engine/Net/MessageDecode.cpp
@@ -35,7 +35,6 @@ ENetPacket* Encode ( const Message& msg )
 
 Message* Decode ( ENetPacket* packet )
 {
-	// todo: this needs security
 	const unsigned char* data = (const unsigned char*)packet->data;
 	uint16_t messageIDLength;
 	memcpy(&messageIDLength, data, 2);
@@ -44,45 +43,33 @@ Message* Decode ( ENetPacket* packet )
 	memcpy(&messageLength, data + 2 + messageIDLength, 4); //find supposed length of message data
 	messageLength = ntohl(messageLength);
 	
-	if(messageIDLength + messageLength == packet->dataLength && messageIDLength < (packet->dataLength - 6)) //if its a valid message, interpret
+	if (messageIDLength + messageLength == packet->dataLength && messageIDLength < (packet->dataLength - 6)) //if its a valid message, interpret
 	{
 		char* messageID = (char*)alloca(messageIDLength + 1);
 		messageID[messageIDLength] = 0;
 		memcpy(messageID, data + 2, messageIDLength);
-		bool goodMessage = true;
-		int i;
 		
-		for(i = 0; i <= messageIDLength; i++) //check messageID contents
+		for (unsigned i = 0; i <= messageIDLength; i++) //check messageID contents
 		{//......................0........9....||...................A........Z....||...................a.........z....||................ _
 			
-			if( (messageID[i] >= 48 && messageID[i]<= 57)  ||  (messageID[i] >= 65 && messageID[i] <= 90)  ||  (messageID[i] >= 97 && messageID[i] <= 122)  ||  (messageID[i] == 95) )
+			if (!((messageID[i] >= 48 && messageID[i]<= 57)  ||  (messageID[i] >= 65 && messageID[i] <= 90)  ||  (messageID[i] >= 97 && messageID[i] <= 122)  ||  (messageID[i] == 95)))
 			{
+				return NULL;
 			}
-			else 
-			{
-				goodMessage = false;
-				break;
-			}
-			
 		}
-		
-		if(goodMessage == true)	
+
+
+		void* messageBuffer = NULL;
+		if (messageLength)
 		{
-			void* messageBuffer = NULL;
-				if (messageLength)
-				{
-					messageBuffer = malloc(messageLength);
-					memcpy(messageBuffer, data + 2 + messageIDLength + 4, messageLength);
-				}
-				std::string messageIDString = std::string(messageID, messageIDLength);
-		
-		
-				Message* result = new Message(messageIDString, messageBuffer, messageLength);
-				free(messageBuffer);
-				return result; //this is only reached if the message passes the tests
+			messageBuffer = alloca(messageLength);
+			memcpy(messageBuffer, data + 2 + messageIDLength + 4, messageLength);
 		}
+		std::string messageIDString = std::string(messageID, messageIDLength);
+		Message* result = new Message(messageIDString, messageBuffer, messageLength);
+		return result; //this is only reached if the message passes the tests
 	}
-		return NULL; // invalid format
+	return NULL; // invalid format
 }
 
 }
