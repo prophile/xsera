3d0dd94eda47755777b3ce3fec1090abef86b1b6
Scott McClaugherty <gamefreak4321@gmail.com>
2009-11-25 20:09:55 -0500

    Added function stubs for all of the action types. Actions are no longer called from a class, instead it is a plain function.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modules/Actions.lua
+++ Resources/Scripts/Modules/Actions.lua
@@ -1,43 +1,49 @@
-class "Action"
 
-function Action:Call(trigger, owner, other)
+function callAction(trigger, owner, other)
+	local id
+	local max = id + trigger.count - 1
+	for id = trigger.id, max do
+		local action = gameData["Actions"][id]
+		actionTable[action.type](action, owner, other)
+	end
 end
---[[
-activate-special-action
-alter-absolute-cash-action
-alter-absolute-location-action
-alter-age-action
-alter-base-type-action
-alter-cloak-action
-alter-condition-true-yet-action
-alter-damage-action
-alter-energy-action
-alter-hidden-action
-alter-location-action
-alter-max-velocity-action
-alter-occupation-action
-alter-offline-action
-alter-owner-action
-alter-special-action
-alter-spin-action
-alter-thrust-action
-alter-velocity-action
-assume-initial-object-action
-change-score-action
-color-flash-action
-computer-select-action
-create-object-action
-create-object-set-dest-action
-declare-winner-action
-die-action
-disable-keys-action
-display-message-action
-enable-keys-action
-land-at-action
-make-sparks-action
-nil-target-action
-no-action
-play-sound-action
-set-destination-action
-set-zoom-action
---]]
+
+actionTable = {
+activate-special-action = function(action, owner, other) end,
+alter-absolute-cash-action = function(action, owner, other) end,
+alter-absolute-location-action = function(action, owner, other) end,
+alter-age-action = function(action, owner, other) end,
+alter-base-type-action = function(action, owner, other) end,
+alter-cloak-action = function(action, owner, other) end,
+alter-condition-true-yet-action = function(action, owner, other) end,
+alter-damage-action = function(action, owner, other) end,
+alter-energy-action = function(action, owner, other) end,
+alter-hidden-action = function(action, owner, other) end,
+alter-location-action = function(action, owner, other) end,
+alter-max-velocity-action = function(action, owner, other) end,
+alter-occupation-action = function(action, owner, other) end,
+alter-offline-action = function(action, owner, other) end,
+alter-owner-action = function(action, owner, other) end,
+alter-special-action = function(action, owner, other) end,
+alter-spin-action = function(action, owner, other) end,
+alter-thrust-action = function(action, owner, other) end,
+alter-velocity-action = function(action, owner, other) end,
+assume-initial-object-action = function(action, owner, other) end,
+change-score-action = function(action, owner, other) end,
+color-flash-action = function(action, owner, other) end,
+computer-select-action = function(action, owner, other) end,
+create-object-action = function(action, owner, other) end,
+create-object-set-dest-action = function(action, owner, other) end,
+declare-winner-action = function(action, owner, other) end,
+die-action = function(action, owner, other) end,
+disable-keys-action = function(action, owner, other) end,
+display-message-action = function(action, owner, other) end,
+enable-keys-action = function(action, owner, other) end,
+land-at-action = function(action, owner, other) end,
+make-sparks-action = function(action, owner, other) end,
+nil-target-action = function(action, owner, other) end,
+no-action = function(action, owner, other) end,
+play-sound-action = function(action, owner, other) end,
+set-destination-action = function(action, owner, other) end,
+set-zoom-action = function(action, owner, other) end
+}
