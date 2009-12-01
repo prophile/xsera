9100e128c4363265954b2ee15e54202d815e8668
Scott McClaugherty <gamefreak4321@gmail.com>
2009-11-30 20:24:01 -0500

    Added stub for scenario loader. Added the ablility to step through object names in the demo level.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modes/Demo4.lua
+++ Resources/Scripts/Modes/Demo4.lua
@@ -4,18 +4,37 @@ import('GlobalVars')
 import('Math')
 import('Scenarios')
 
-gameData = dofile("./Xsera.app/Contents/Resources/Config/data.lua") --[FIX] this is A) not cross platform in ANY way shape or form B) an ugly way of fixing it.
 
+cid = 0
+
+function init()
+	physics.open(0.6)
+	loadingEntities = true
+	gameData = dofile("./Xsera.app/Contents/Resources/Config/data.lua") --[FIX] this is A) not cross platform in ANY way shape or form B) an ugly way of fixing it.
+	loadingEntities = false
+	scen = LoadScenario(1)
+end
 
 function key( k )
+	print(k)
 	if k == "q" then
 		mode_manager.switch("MainMenu")
+	elseif k =="w" then
+		cid = cid + 1
+	elseif k == "s" then
+		cid = cid - 1
+	elseif k == "return" then
+		
 	end
 end
 
+function update()
+
+end
 
 function render()
 	graphics.begin_frame()
-	
+	graphics.draw_text("Current id: " .. cid,"CrystalClear","left",-400,200,60)
+	graphics.draw_text(gameData["Objects"][cid].name,"CrystalClear","left",-400,120,30)
 	graphics.end_frame()
 end
---
+++ Resources/Scripts/Modules/Scenarios.lua
@@ -0,0 +1,4 @@
+function LoadScenario(id)
+	local scen = gameData["Scenarios"][id]
+	return scen
+end
