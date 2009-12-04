c289606c3fce5566223ab69528586d062c31b66a
Scott McClaugherty <gamefreak4321@gmail.com>
2009-12-03 20:30:25 -0500

    Senarios and Objects are now coppied correctly
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modules/ObjectLoad.lua
+++ Resources/Scripts/Modules/ObjectLoad.lua
@@ -1,9 +1,10 @@
 function NewObject(id)
-	local newObj = gameData["Objects"][id]
+	local newObj = deepcopy(gameData["Objects"][id])
 	newObj.sprite = newObj["sprite-id"]
 	if newObj.mass == nil then
 		newObj.mass = 1000.0 --We should add a way for objects to me immobile
 	end
+	print("Creating new " .. newObj.name)
 	newObj.physics = physics.new_object(newObj.mass)
 	newObj.physics.angular_velocity = 1.00
 	return newObj
--- Resources/Scripts/Modules/Scenarios.lua
+++ Resources/Scripts/Modules/Scenarios.lua
@@ -1,13 +1,14 @@
 function LoadScenario(id)
-	local scen = gameData["Scenarios"][id]
+	local scen = deepcopy(gameData.Scenarios[id])
 	scen.objects = {}
 	local datId
 	local max = scen.initial.id + scen.initial.count - 1
 	local ctr = 0
+	
 	for datId = scen.initial.id, max do
 		local state = gameData.InitialObject[datId]
 		local new = NewObject(state.type)
-		print(new.name)
+
 		new.physics.position = state.location
 		scen.objects[ctr] = new
 		ctr = ctr + 1
