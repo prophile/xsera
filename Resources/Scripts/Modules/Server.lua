256a637ffc7bb4f604b67229165b06aec5e3117c
Adam Hintz <adam.0.dev@gmail.com>
2010-01-13 07:13:11 -0800

    Numerous corrections to the Lua client-server view of things.
    Signed-off-by: Adam Hintz <adam.0.dev@gmail.com>

--- Resources/Scripts/Modules/Server.lua
+++ Resources/Scripts/Modules/Server.lua
@@ -1,23 +1,22 @@
 -- These are functions that Lua needs if this computer is a server
 
-local msgQueue = {} -- outgoing messages
 local cmdQueue = {} -- incoming messages
 
 function sendToClients()
 	-- return a boolean indicating successful sending to the client(s) or not
 		-- on a local game, sendToClients should work in coordination with Client.lua
-	-- this should only execute on the pre-determined frequency (12Hz?) for multiplayer games
-		-- does this mean that this function should check to see if the alloted time is up?
 end
 
 function receivedFromClients()
 	-- return a boolean on whether or not a message has come in from any client(s) since the last time checked
+	-- if true, put the messages into cmdQueue
 end
 
 function readClientMsgs()
 	-- read from cmdQueue what the client(s) sent
 end
 
-function addMsg(msg)
-	msgQueue[#msgQueue + 1] = msg
+function addServerMsg(msg)
+	-- process the message and send it to the C++ side
+	-- for singleplayer, do not send to C++, instead send it to...?
 end
