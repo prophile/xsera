79fc341f9a073f8305c3c50084d6374bf85207a3
Scott McClaugherty <gamefreak4321@gmail.com>
2010-02-01 00:41:26 -0500

    Ships now attack targeted non-allied ships when sufficiently close.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modules/PilotAI.lua
+++ Resources/Scripts/Modules/PilotAI.lua
@@ -3,13 +3,26 @@ import('Math')
 function Think(object)
 	
 	local target = object.ai.objectives.target or object.ai.objectives.dest
-
+	local dist
 	if target ~= nil then
-		local dist = find_hypot(object.physics.position, target.physics.position)
-		if dist > 200 then --[HARDCODE]
+		dist = find_hypot(object.physics.position, target.physics.position)
+		if dist < 350
+		and target.ai.owner ~= object.ai.owner
+		and target.base.attributes["hated"] == true then
+			object.ai.mode = "engage"
+		elseif dist > 200 and object.ai.mode == "wait" then
 			object.ai.mode = "goto"
-		else
+		elseif dist < 150 and object.ai.mode == "goto" then
 			object.ai.mode = "wait"
+		end
+	else
+		object.ai.mode = "wait"
+	end
+		
+		if object.ai.mode ~= "engage" then
+			object.control.beam = false
+			object.control.pulse = false
+			object.control.special = false
 		end
 		
 		if object.ai.mode == "wait" then
@@ -20,14 +33,36 @@ function Think(object)
 		elseif object.ai.mode == "goto" then
 			object.control.accel = true
 			object.control.decel = false
-			TurnToTarget(object,target)
+			TurnToward(object,target)
+		elseif object.ai.mode == "evade" then
+			TurnAway(object, target)
+		elseif object.ai.mode == "engage" then
+			object.control.accel = true
+			object.control.decel = false
+			object.control.beam = true
+			object.control.pulse = true
+			object.control.special = true
+			TurnToward(object,target)
 		end
-	end
+		
+--[[	else	
+		object.control.accel = false
+		object.control.decel = true
+		object.control.left = false
+		object.control.right = false
+		object.control.beam = false
+		object.control.pulse = false
+		object.control.special = false
+		object.ai.mode = "wait"]]
+		
+--	end
 end
 
+function TurnAway(object, target)
+	--NO CODE!!!
+end
 
-
-function TurnToTarget(object, target)
+function TurnToward(object, target)
 
 	local ang = find_angle(target.physics.position,object.physics.position) - object.physics.angle
 
@@ -40,7 +75,7 @@ function TurnToTarget(object, target)
 		object.control.decel = true
 	end]]
 	
-	if math.abs(ang) < 0.1 then
+	if math.abs(ang) < 0.2 then
 		object.control.left = false
 		object.control.right = false
 	elseif ang <= math.pi then
