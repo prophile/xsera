185e1b4ce4076ad7a61f3faefcd6b2d7f7ba4437
Scott McClaugherty <gamefreak4321@gmail.com>
2010-05-08 20:10:17 -0400

    Implemented turbo mode.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modes/Demo4.lua
+++ Resources/Scripts/Modes/Demo4.lua
@@ -72,6 +72,9 @@ function update()
 
 	if menu_display == nil and consoleDraw == false then
 		realTime = realTime + dt
+		if keyboard[4][7].active == true then
+			dt = dt * 8
+		end
 
 		KeyDoActivated()
 		
