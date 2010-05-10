b71001c09b6cbe9eaec64dfd1cd06b92bb46e51b
Scott McClaugherty <gamefreak4321@gmail.com>
2010-05-09 21:28:53 -0400

    Restored zoom sound functionality, a bit of cleanup.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modules/Camera.lua
+++ Resources/Scripts/Modules/Camera.lua
@@ -3,8 +3,7 @@ WINDOW.width, WINDOW.height = window.size()
 
 panels = { left = { width = 128, height = 768, center = { x = -WINDOW.width / 2, y = 0 } }, right = { width = 32, height = 768, center = { x = WINDOW.width / 2, y = 0 } } }
 cameraRatio = { current = 1, num = 2, target = 1 }
---cameraRatio = 1
---cameraRatioTarget = 1
+
 aspectRatio = WINDOW.width / WINDOW.height
 camera = { w = WINDOW.width / cameraRatio.current, h }
 camera.h = camera.w / aspectRatio
@@ -75,6 +74,7 @@ CAMERA_DYNAMIC_THRESHOLD = 6
 -- same as the target, and adjusting if not? [ADAM, TODO]
 
 function CameraInterpolate(dt)
+	local oldRatio = cameraRatio.current
 	local zoomGoal
 	if cameraRatio.target < CAMERA_DYNAMIC_THRESHOLD then
 		--Normal scaling
@@ -83,12 +83,16 @@ function CameraInterpolate(dt)
 		--Dynamic scaling
 		zoomGoal = CAMERA_RATIO_OPTIONS[cameraRatio.target]()
 	end
---[ [
+
 	local zoomTime = math.max(math.abs(math.log(zoomGoal/cameraRatio.current)/math.log(2)),1)
 	if zoomTime ~= 0 then
 	cameraRatio.current = cameraRatio.current + (zoomGoal-cameraRatio.current)*(zoomTime*dt)
 	end
---]]
+
+	if (cameraRatio.current < 1 / 4 and oldRatio > 1 / 4)
+	or (cameraRatio.current > 1 / 4 and oldRatio < 1 / 4) then
+		sound.play("ZoomChange")
+	end
 
 
 	print(cameraRatio.current)
@@ -98,14 +102,6 @@ function CameraInterpolate(dt)
 	arrowLength = ARROW_LENGTH / cameraRatio.current
 	arrowVar = ARROW_VAR / cameraRatio.current
 	arrowDist = ARROW_DIST / cameraRatio.current
-
---[[	if (cameraRatio.current < 1 / 4 and cameraRatioOrig > 1 / 4) or (cameraRatio.current > 1 / 4 and cameraRatioOrig < 1 / 4) then
-		if soundJustPlayed == false then
-			sound.play("ZoomChange")
-			soundJustPlayed = true
-		end
-	end
---]]
 
 	--[==[
 	if cameraRatio. >= CAMERA_DYNAMIC_THRESHOLD
