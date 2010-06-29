cbba174f293d17e8d361ac49036320a8bad011d9
Alastair Lynn <arplynn@gmail.com>
2010-06-29 15:57:32 +0100

    Toned down the specular highlights a little.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- Resources/Scripts/Tests/3DTest.lua
+++ Resources/Scripts/Tests/3DTest.lua
@@ -6,6 +6,6 @@ end
 
 function render()
 	graphics.begin_frame()
-		graphics.draw_3d_ambient("Teapot", {x=0, y=0}, 120.0, mode_manager.time(), mode_manager.time() * 5.0)
+		graphics.draw_3d_ambient("Cube", {x=0, y=0}, 60.0, mode_manager.time(), mode_manager.time() * 5.0)
 	graphics.end_frame()
 end
