e7534db1965cdabda4856a4a2240be5fd0a3ef39
Alastair Lynn <arplynn@gmail.com>
2010-07-04 04:40:42 +0100

    Added an Obish cruiser object.
    Yes, you did read that right.
    
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- Resources/Scripts/Tests/3DTest.lua
+++ Resources/Scripts/Tests/3DTest.lua
@@ -3,18 +3,34 @@ function key(k)
 		mode_manager.switch("Xsera/MainMenu")
 	elseif k == "1" then
 		object = "Cube"
+		fullOrbit = true
 	elseif k == "2" then
 		object = "Cabinet"
+		fullOrbit = true
 	elseif k == "3" then
 		object = "Teapot"
+		fullOrbit = true
+	elseif k == "4" then
+		object = "ObishCruiser"
+		fullOrbit = false
+	elseif k == "w" then
+		scale = scale + 10.0
+	elseif k == "s" then
+		scale = scale - 10.0
 	end
 end
 
 object = "Cube"
+scale = 60.0
+fullOrbit = true
 
 function render()
 	graphics.begin_frame()
 		graphics.draw_starfield()
-		graphics.draw_3d_ambient(object, {x=0, y=0}, 60.0, mode_manager.time(), mode_manager.time() * 5.0)
+		if fullOrbit then
+			graphics.draw_3d_ambient(object, {x=0, y=0}, scale, mode_manager.time(), mode_manager.time() * 5.0)
+		else
+			graphics.draw_3d_ambient(object, {x=0, y=0}, scale, mode_manager.time(), 0.0)
+		end
 	graphics.end_frame()
 end
