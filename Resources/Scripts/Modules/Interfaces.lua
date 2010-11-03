65fd40beab0f8949bfef59184c9d5355ae828617
Scott McClaugherty <gamefreak4321@gmail.com>
2010-11-03 17:22:33 -0400

    Added missing `end`
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Modules/Interfaces.lua
+++ Resources/Scripts/Modules/Interfaces.lua
@@ -699,6 +699,7 @@ function DrawTargetBox(object, isControl)
 	-- [INCOMPLETE]
 	if object.base.weapon.beam.id ~= -1 then
 		graphics.draw_text(gameData.Objects[object.base.weapon.beam.id].shortName, MAIN_FONT, "left", { x = panels.left.center.x - 6, y = 20 - off }, 14)
+	end
 	if object.base.weapon.pulse.type ~= -1 then
 		graphics.draw_text(gameData.Objects[object.base.weapon.pulse.id].shortName, MAIN_FONT, "left", { x = panels.left.center.x - 6, y = 30 - off }, 14)
 	end
