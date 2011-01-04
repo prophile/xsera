3524ce46d44cf587b7ed6004980b598e6ea85c8e
Scott McClaugherty <gamefreak4321@gmail.com>
2011-01-04 15:52:55 -0500

    Fixes to the test.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Resources/Scripts/Tests/JaggedList.lua
+++ Resources/Scripts/Tests/JaggedList.lua
@@ -1,5 +1,30 @@
+__idmt = {
+    __lt = function(a, b)
+        if a[1] == b[1] then
+            if a[2] == nil then print(debug.traceback("A")) end
+            if b[2] == nil then print(debug.traceback("B")) end
+            return a[2] < b[2]
+        else
+            return a[1] < b[1]
+        end
+    end;
+    __le = function(a, b)
+        if a[1] == b[1] then
+            return a[2] <= b[2]
+        else
+            return a[1] <= b[1]
+        end
+    end;
+    __eq = function(a, b)
+        return a[1] == b[1] and a[2] == b[2]
+    end;
+    __tostring = function(a)
+        return (a[1] or "nil") .. ", " .. (a[2] or "nil")
+    end;
+}
+
 function ObjectIterator(list)
-    local function jaggedObjectListOterator(list, index)
+    local function jaggedListIterator(list, index)
         if index[2] == nil then
             index[1] = next(list, index[1])
             if index[1] == nil then
@@ -8,10 +33,14 @@ function ObjectIterator(list)
         end
         local i, k = next(list[index[1]], index[2])
         index[2] = i
-
+        if index[2] == nil then
+            return jaggedListIterator(list, index)
+        end
         return index, k
     end
-    return jaggedObjectListOterator, list, {nil, nil}
+    local nlt = {nil, nil}
+    setmetatable(nlt, __idmt)
+    return jaggedListIterator, list, nlt
 end
 
 list = {
@@ -25,7 +54,7 @@ list = {
 
 for a, b in ObjectIterator(list) do
     if b ~= nil then
-        print(b)
+        print(a,": ", b)
     end
 end
 
